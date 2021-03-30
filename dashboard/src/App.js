d87db485ead508e33266836eaa62d56594ffd368
Emmanuel Nwachi <egreatness1@gmail.com>
2021-03-30 12:35:26 +0100

    clean up

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,24 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ConfirmPassword from './Components/ConfirmPassword';
 import ForgotPassword from './Components/ForgotPassword';
 import AppDashboard from './pages/Dashboard'
-// import Dashboard from "./pages/Dashboard";
-// import Invest from "./pages/Invest";
-// import RequestMoney from "./pages/RequestMoney";
-// import TransactionLog from "./pages/TransactionLog";
-// import Settings from "./pages/Settings";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <AppDashboard /> */}
         <Switch>
           <Route path="/" exact component={AppDashboard} />
           <Route path="/forgotPassword" component={ForgotPassword} />
           <Route path="/confirmPassword" component={ConfirmPassword} />
-
-          {/* <ForgotPassword />
-          <ConfirmPassword /> */}
         </Switch>
       </BrowserRouter>
     </>
